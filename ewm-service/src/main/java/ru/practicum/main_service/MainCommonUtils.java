f8c13441b3c98f7fde4d573feb930673ad279b4d
isokovikov <i.sokovikov@gs1ru.org>
2024-06-04 23:16:19 +0300

    fix: а что не так

--- ewm-service/src/main/java/ru/practicum/main_service/MainCommonUtils.java
+++ ewm-service/src/main/java/ru/practicum/main_service/MainCommonUtils.java
@@ -5,8 +5,8 @@ import java.time.format.DateTimeFormatter;
 public class MainCommonUtils {
     public static final String DT_FORMAT = "yyyy-MM-dd HH:mm:ss";
     public static final DateTimeFormatter DT_FORMATTER = DateTimeFormatter.ofPattern(DT_FORMAT);
-    public static final String PAGE_DEFAULT_FROM = "0";
-    public static final String PAGE_DEFAULT_SIZE = "10";
+//    public static final String PAGE_DEFAULT_FROM = "0";
+//    public static final String PAGE_DEFAULT_SIZE = "10";
     public static final int MIN_LENGTH_ANNOTATION = 20;
     public static final int MAX_LENGTH_ANNOTATION = 2000;
     public static final int MIN_LENGTH_DESCRIPTION = 20;
